6f1d4e1e4212f98a936d3817426efa7f5a51acd6
Howaida Sayed <howaidasayed95@gmail.com>
2026-08-09 23:42:37 +0300

    fix: setup axios types

--- src/router/ProtectedRoute.tsx
+++ src/router/ProtectedRoute.tsx
@@ -1,13 +1,14 @@
+import { type ReactNode } from 'react';
 import { Navigate } from "react-router-dom";
 import { ROUTES } from "@/constants";
 
-const ProtectedRoute= ({ children }) => {
+const ProtectedRoute = ({ children }: { children: ReactNode }) => {
     const token = true; // Replace with your authentication logic
 
     if (!token) {
-        return <Navigate to={ROUTES.LOGIN} replace />;
+        return <Navigate to={ROUTES.SIGN_IN} replace />;
     }
 
-    return children;
+    return <>{children}</>;
 }
 export default ProtectedRoute;
